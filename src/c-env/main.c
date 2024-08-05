d76606a3213d7e19200ed857273eb8c9b352f30a
so3narahara <saitoso.0319@gmail.com>
2024-08-06 00:34:12 +0900

    fix:memory address

--- src/c-env/main.c
+++ src/c-env/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a,b;
+    a = 10,b = 20;
+    printf("a address:%p\n",&a);
+    printf("b address:%p\n",&b);
     return 0;
 }
